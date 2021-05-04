c906bcf9febf53029d6d7116ed5c207e37c5f663
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2021-05-04 00:32:07 -0400

    🐛 fix: `sapper.js` and `svelte-kit.js` were swapped

--- projects/svelte-localstorage/svelte-kit.js
+++ projects/svelte-localstorage/svelte-kit.js
@@ -1,3 +1,4 @@
+import { browser } from "$app/env";
 import { localStorageStore as base } from "./base";
 
 /**
@@ -6,4 +7,4 @@ import { localStorageStore as base } from "./base";
  * @param {Item} initial The initial value of the writable store
  * @returns {import("svelte/store").Writable<Item>} A writable store that synchronizes to localStorage
  */
-export const localStorageStore = (key, initial) => base(key, initial, process.browser);
+export const localStorageStore = (key, initial) => base(key, initial, browser);
