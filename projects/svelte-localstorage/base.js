eab9f434d903e56a0ea7d7cd9715b82d22b21cb2
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2022-07-20 22:08:17 -0400

    🧹 chore: refactor to make more sense, and add more precise errors

--- projects/svelte-localstorage/base.js
+++ projects/svelte-localstorage/base.js
@@ -18,57 +18,72 @@ export const localStorageStore = (
 ) => {
   let currentValue = initial;
 
-  const { set: setStore, ...readableStore } = writable(initial, () => {
+  /**
+   * @param {import("svelte/store").Writable<Item>["set"]} setStore
+   * @param {Item} value
+   */
+  const syncCurrentValue = (setStore, value) => {
+    setStore(value);
+    currentValue = value;
+  };
+
+  /** @param {string | null} localValue */
+  const parseFromLocalStorage = (localValue) => {
+    if (localValue === null) return initial;
+
+    try {
+      return deserialize(localValue);
+    } catch (error) {
+      console.error(
+        `localStorage's value for \`${key}\` (\`${localValue}\`) could not be deserialized with ${deserialize} because of ${error}, so the initial value \`${initial}\` will be used instead`
+      );
+      return initial;
+    }
+  };
+
+  const { set: setStore, subscribe } = writable(initial, (setStore) => {
     if (browser) {
-      getAndSetFromLocalStorage();
+      /** @type {string | null} */
+      let localStorageValue = null;
+      try {
+        localStorageValue = localStorage.getItem(key);
+      } catch (error) {
+        console.error(
+          `the \`${key}\` store's value could not be restored from localStorage because of ${error}, so the initial value \`${initial}\` will be used instead`
+        );
+      }
+
+      syncCurrentValue(setStore, parseFromLocalStorage(localStorageValue));
 
       /** @param {StorageEvent} event */
-      const updateFromStorageEvents = (event) => {
-        if (event.key === key) getAndSetFromLocalStorage();
+      const setFromStorageEvents = (event) => {
+        if (event.key === key)
+          syncCurrentValue(setStore, parseFromLocalStorage(event.newValue));
       };
-      window.addEventListener("storage", updateFromStorageEvents);
-      return () =>
-        window.removeEventListener("storage", updateFromStorageEvents);
+      window.addEventListener("storage", setFromStorageEvents);
+      return () => window.removeEventListener("storage", setFromStorageEvents);
     }
   });
 
   // Set both localStorage and this Svelte store
   /** @type {import("svelte/store").Writable<Item>["set"]} */
   const set = (value) => {
-    setStore(value);
-    currentValue = value;
+    syncCurrentValue(setStore, value);
 
     try {
-      localStorage.setItem(key, serialize(value));
-    } catch (error) {
-      console.error(
-        `the \`${key}\` store's new value \`${value}\` could not be persisted to localStorage because of ${error}`
-      );
-    }
-  };
+      const serialized = serialize(value);
 
-  // Synchronize the Svelte store with localStorage
-  const getAndSetFromLocalStorage = () => {
-    let localValue = null;
-    try {
-      localValue = localStorage.getItem(key);
-    } catch (error) {
-      console.error(
-        `the \`${key}\` store's value could not be restored from localStorage because of ${error}`
-      );
-    }
-
-    if (localValue === null) set(initial);
-    else {
       try {
-        const parsed = deserialize(localValue);
-        setStore(parsed);
-        currentValue = parsed;
+        localStorage.setItem(key, serialized);
       } catch (error) {
         console.error(
-          `localStorage's value for \`${key}\` (\`${localValue}\`) could not be parsed as JSON because of ${error}`
+          `the \`${key}\` store's new value \`${value}\` (which serialized to \`${serialized}\`) could not be persisted to localStorage because of ${error}`
         );
       }
+    } catch (error) {
+      console.error(
+        `the \`${key}\` store was set to \`${value}\`, but this could not be serialized with ${serialize} because of ${error}, so it won't be persisted to localStorage`
+      );
     }
   };
 
@@ -77,5 +92,5 @@ export const localStorageStore = (
     set(fn(currentValue));
   };
 
-  return { ...readableStore, set, update };
+  return { set, subscribe, update };
 };
