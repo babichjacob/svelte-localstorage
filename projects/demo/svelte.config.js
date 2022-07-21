f101be0a04a025b5b18b97f29d29f055f4a76cc6
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2022-07-20 21:28:48 -0400

    🧹 chore: upgrade dependencies to their latest versions

--- projects/demo/svelte.config.js
+++ projects/demo/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,16 +6,6 @@ const config = {
     adapter: adapter({
       fallback: "fallback.html",
     }),
-
-    vite: {
-      optimizeDeps: {
-        exclude: ["@babichjacob/svelte-localstorage"],
-      },
-      // @ts-ignore
-      ssr: {
-        noExternal: ["@babichjacob/svelte-localstorage"],
-      },
-    },
   },
 };
 
